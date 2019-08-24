21d9aa611a33f7a6433bd058650c6ac4390de805
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-08-24 12:34:41 +0200

    add google analytics

--- src/MicroUrl/ClientApp/src/app/app.component.ts
+++ src/MicroUrl/ClientApp/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { Router, RouterEvent, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationEnd) {
+        ga("set", "page", event.urlAfterRedirects);
+        ga("send", "pageview");
+      }
+    });
+  }
+}
