834e56d2a747b377e97804327aea44dc1a2795a3
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-08-25 23:53:30 +0200

    add list of buttons in component

--- src/ClientApp/src/app/app.component.ts
+++ src/ClientApp/src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 
+interface NavigationOption {
+  route: string;
+  name: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public navigationEntries: NavigationOption[] = [
+    {
+      icon: 'link',
+      name: 'Link',
+      route: '/'
+    },
+    {
+      icon: 'code',
+      name: 'Editor',
+      route: '/editor'
+    },
+    {
+      icon: 'compare',
+      name: 'Diff',
+      route: '/diff'
+    },
+    {
+      icon: 'trending_up',
+      name: 'Stats',
+      route: '/stats'
+    }
+  ];
+
   constructor(router: Router) {
     router.events.subscribe((event: RouterEvent) => {
       if (event instanceof NavigationEnd) {
