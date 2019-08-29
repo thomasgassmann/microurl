db805b0177d3796d254d7c5889a7cff72fbafbbb
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-08-29 21:01:22 +0200

    add basic form for stats

--- src/ClientApp/src/app/components/stats/stats.component.ts
+++ src/ClientApp/src/app/components/stats/stats.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.scss']
 })
-export class StatsComponent implements OnInit {
+export class StatsComponent {
 
-  constructor() { }
+  public statsForm: FormGroup;
 
-  ngOnInit() {
+  constructor(formBuilder: FormBuilder) {
+    this.statsForm = formBuilder.group({
+      key: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9]{1,}')])
+    });
+  }
+
+  public loadStats(): void {
+    const key = this.statsForm.controls['key'].value;
   }
 
 }
