44e115856a3f2f65463fb16cd50808a3b2ad8689
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-09-03 23:57:00 +0200

    use new api in diff too, allow to edit both

--- src/ClientApp/src/app/components/diff/diff.component.ts
+++ src/ClientApp/src/app/components/diff/diff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-diff',
@@ -11,6 +12,18 @@ export class DiffComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public editorCreator(domElement: HTMLElement): monaco.editor.IEditor {
+    const editor = monaco.editor.createDiffEditor(domElement, {
+      theme: 'vs-dark',
+      originalEditable: true,
+    });
+    editor.setModel({
+      original: monaco.editor.createModel('', 'javascript'),
+      modified: monaco.editor.createModel('', 'javascript')
+    });
+    return editor;
   }
 
 }
