33c12875e86ae3f6aeddd3f881c8b0b9f01ff8ef
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-11-10 15:13:05 +0100

    remove unused settings from aggregate

--- src/MicroUrl.Web/Infrastructure/Settings/UrlSettings.cs
+++ src/MicroUrl.Web/Infrastructure/Settings/UrlSettings.cs
@@ -2,8 +2,6 @@ namespace MicroUrl.Infrastructure.Settings
 {
     public class MicroUrlSettings
     {
-        public DataStorageSettings Storage { get; set; }
-
         public string AnalyticsId { get; set; }
     }
 }
